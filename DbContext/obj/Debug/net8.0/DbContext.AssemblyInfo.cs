14d8938f889c6e8fce1f36b069845c7467e3287f
Martin <martin_lenart@icloud.com>
2024-05-14 14:57:27 +0200

    update

--- DbContext/obj/Debug/net8.0/DbContext.AssemblyInfo.cs
+++ DbContext/obj/Debug/net8.0/DbContext.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DbContext")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66afece3c3ea022a82de62dabca49fec1399d3f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75336644f9ee6f6e8e77a34a9db5bf23e8ec0f13")]
 [assembly: System.Reflection.AssemblyProductAttribute("DbContext")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DbContext")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
